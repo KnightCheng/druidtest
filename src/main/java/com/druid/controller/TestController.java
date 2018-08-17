227499690620a9a767961cbf2aedade035cc3292
LearntoAndroid <1105537272@qq.com>
2018-08-17 18:09:30 +0800

    mybatis generator maven自动生成配置

--- src/main/java/com/druid/controller/TestController.java
+++ src/main/java/com/druid/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.druid.controller;
 
+import com.druid.model.SysAcl;
+import com.druid.service.SysAclService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,11 +12,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/test")
 @Slf4j
 public class TestController {
+
+    @Autowired
+    private SysAclService sysAclService;
+
+
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
 
         return "hello permission";
+    }
+
+    @RequestMapping("/model")
+    public SysAcl model(){
+        return sysAclService.selectByPrimaryKey(1);
     }
 }
 
